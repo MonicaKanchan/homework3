58ce15a0bffedc5e4152604f427d8d88e6fe3e9e
monica nagendra kanchan <mk758@afsconnect1.njit.edu>
2017-09-28 12:01:13 -0400

    merge array added

--- public_html/homework3/index.php
+++ public_html/homework3/index.php
@@ -8,6 +8,9 @@ $obj->printthis($text);
 $array = array(1,2,3,4,5,6,7);
 $obj->printArray($array);
 
+$array1 = array("Monica","Nehal");
+$array2 = array("Shriya","Ankita","Meetali");
+$obj->mergeArray($array1,$array2);
 class main{
 public function _construct(){
 echo 'hello i\'m an object </br>';
@@ -16,12 +19,29 @@ echo 'hello i\'m an object </br>';
 public function printthis($text){
 echo'<h1>print function demo </h1>';
 print($text);
-echo '<hr>;
+echo '<hr>';
+}
+public function printArray($array){
+echo '<h1>array print function</h1>';
+print_r($array);
+echo'<hr>';
 }
 
+public function mergeArray($array1,$array2){
+echo '<h1>array merge function</h1>';
+echo '</br>Array 1</br>';
+print_r($array1);
+echo '</br>Array 2</br>';
+print_r($array2);
+echo '</br>Merged array</br>';
+$result= array_merge($array1,$array2);
+print_r($result);
+echo '<hr>';
+}
 public function _destruct() {
-echo '<br> i\'m done';
+echo '</br> i\'m done';
 }
 }
 ?>
-
+</body>
+</html>

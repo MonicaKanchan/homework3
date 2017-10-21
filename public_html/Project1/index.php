5dfed08df5a3e54cc2167f2b029b1158cd4dac1a
monica nagendra kanchan <mk758@afsconnect1.njit.edu>
2017-10-21 16:18:23 -0400

    added index file

--- public_html/Project1/index.php
+++ public_html/Project1/index.php
@@ -3,4 +3,39 @@
 ini_set('display errors', 'On');
 error_reporting(E_ALL);
 
+class Manage
+{
+public static function autoload($class)
+{
+include $class . '.php';
+}
+}
+
+spl_autoload_register(array('Manage', 'autoload'));
+
+$obj = new main();
+
+class main
+{
+public function __construct()
+{
+$pageRequest = 'homepage';
+if (isset($_REQUEST['page']))
+{
+$pageRequest = $_REQUEST['page'];
+}
+$page = new $pageRequest;
+
+if($_SERVER['REQUEST_METHOD'] == 'GET')
+{
+$page->get();
+}
+else
+{
+$page->post();
+}
+}
+}
+
+
 

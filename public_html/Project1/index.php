ded66bf9d0a91695284da3bd4f84565ac1d63df8
monica nagendra kanchan <mk758@afsconnect1.njit.edu>
2017-10-21 16:37:19 -0400

    added abstract class

--- public_html/Project1/index.php
+++ public_html/Project1/index.php
@@ -37,5 +37,31 @@ $page->post();
 }
 }
 
+abstract class page
+{
+protected $html;
+
+public function _construct()
+{
+$this->html .= '<html>';
+$this->html .= '<link rel="stylesheet" href="styles.css">';
+$this->html .= '<body>';
+}
+public function _destruct()
+{
+$this->html .= '</body></html>';
+stringFunctions::printThis($this->html);
+}
+
+public function get()
+{
+echo 'default get message';
+}
+
+public function post()
+{
+print_r($_POST);
+}
+}
 
 
